42a36e874a230cf046f4f22e2d52e0e0eed93938
haha7 <casper24723902@gmail.com>
2025-02-27 01:32:58 +0800

    Modify function q_remove_head
    Replace '!head->prev == prev'
    with 'list_empty(head)' for consistency with q_remove_head.
    
    Change-Id: I72f9bc3a14fd8ae63ea6bed50ff5a440bac84dec

--- queue.c
+++ queue.c
@@ -79,7 +79,7 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head || head->prev == head) {
+    if (!head || list_empty(head)) {
         return NULL;
     }
     return q_remove_head(head->prev->prev, sp, bufsize);
