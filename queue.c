d7ea72725853246ba7045268e84085a2b71b982d
haha7 <casper24723902@gmail.com>
2025-02-27 01:43:48 +0800

    Implement function q_delete_mid
    Use slow and fast pointers to find and free
    the middle node in the queue. If the number of nodes
    is even, remove the node just before the center.
    The while loop terminates when the fast pointer
    is about to complete a full cycle back to head.
    
    Change-Id: Id9f6f7349223b841ff84f4765a6996d0a4d5e812

--- queue.c
+++ queue.c
@@ -102,7 +102,19 @@ int q_size(struct list_head *head)
 /* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
-    // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head *slow = head->next, *fast = head->next;
+    while (fast != head && fast->next != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    list_del(slow);
+    element_t *element = list_entry(slow, element_t, list);
+    free(element->value);
+    free(element);
     return true;
 }
 
